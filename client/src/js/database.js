614ad014ab348d26a57b6bcd0123d30c8d00792e
Jacob Postill <postilljacob@gmail.com>
2023-03-20 20:28:06 -0400

    worked on databases

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -11,11 +11,20 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
-
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const database = await initdb('jate', 1);
+  const transaction = database.transaction('jate', 'readwrite');
+  const jateStoreage = transaction.objectStore('jate');
+  await jateStoreage.put({Text: content, id: 1});
+  console.log('Data added to Jate')
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
-initdb();
+export const getDb = async () => {
+  const database = await initdb('jate', 1);
+  const transaction = database.transaction('jate', 'readonly');
+  const jateStoreage = transaction.objectStore('jate');
+  const final = await jateStoreage.getAll();
+  return final.value;
+};
